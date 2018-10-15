1ea0838902e55899aa425174d5373d5458a76e8b
Joseph Muia <joseph.b.muia@gmail.com>
2018-10-14 20:40:33 -0400

    verbose logging for health checks

--- health_check.go
+++ health_check.go
@@ -18,9 +18,11 @@ func (hc *TCPHealthCheck) Check() error {
 	// TODO: TCP half-open connection.
 	conn, err := net.DialTimeout("tcp", hc.addr, hc.timeout)
 	if err != nil {
+		logger.Infof("%s failed tcp health check", hc.addr)
 		return err
 	}
 	conn.Close()
+	logger.Infof("%s passed tcp health check", hc.addr)
 	return nil
 }
 
