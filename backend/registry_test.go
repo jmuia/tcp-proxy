57dccb9c29e6777ba4507b0d49fca90256ad9842
Joseph Muia <joseph.b.muia@gmail.com>
2018-10-17 00:05:24 -0400

    test registry add, remove, evict

--- backend/registry_test.go
+++ backend/registry_test.go
@@ -16,6 +16,7 @@ func TestUpdates(t *testing.T) {
 		HealthyThreshold:   1,
 	}
 	registry := NewRegistry(cfg)
+	defer registry.EvictAll()
 
 	updatec := make(chan Backend)
 	registry.RegisterUpdateListener(func(backend *Backend) {
@@ -64,4 +65,51 @@ func TestUpdates(t *testing.T) {
 			updateState.String(),
 		)
 	}
+}
+
+func TestAddingAndRemoving(t *testing.T) {
+	cfg := health.HealthCheckConfig{
+		Timeout:            10 * time.Millisecond,
+		Interval:           1 * time.Minute,
+		UnhealthyThreshold: 1,
+		HealthyThreshold:   1,
+	}
+	registry := NewRegistry(cfg)
+
+	// Add backends.
+	registry.Add("localhost:12345")
+	registry.Add("localhost:54321")
+	registry.Add("localhost:19293")
+	snapshot := registry.Snapshot()
+	if len(snapshot) != 3 {
+		t.Errorf("expected 3 backends in the registry, was %d: %v", len(snapshot), snapshot)
+	}
+	assertContains(t, snapshot, "localhost:12345")
+	assertContains(t, snapshot, "localhost:54321")
+	assertContains(t, snapshot, "localhost:19293")
+
+	// Remove backends.
+	registry.Remove("localhost:54321")
+	snapshot = registry.Snapshot()
+	if len(snapshot) != 2 {
+		t.Errorf("expected 2 backends in the registry, was %d: %v", len(snapshot), snapshot)
+	}
+	assertContains(t, snapshot, "localhost:12345")
+	assertContains(t, snapshot, "localhost:19293")
+
+	// Evict all backends.
+	registry.EvictAll()
+	snapshot = registry.Snapshot()
+	if len(snapshot) != 0 {
+		t.Errorf("expected 0 backends in the registry, was %d: %v", len(snapshot), snapshot)
+	}
+}
+
+func assertContains(t *testing.T, snapshot []Backend, addr string) {
+	for _, backend := range snapshot {
+		if backend.Addr() == addr {
+			return
+		}
+	}
+	t.Errorf("expected to find backend with addr %s in %v", addr, snapshot)
 }
