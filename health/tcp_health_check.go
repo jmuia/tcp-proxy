ed563d149ecf59e5935d4ea6dcb4240816d22d3a
Joseph Muia <joseph.b.muia@gmail.com>
2018-10-18 00:15:15 -0400

    remove noisy logs

--- health/tcp_health_check.go
+++ health/tcp_health_check.go
@@ -3,8 +3,6 @@ package health
 import (
 	"net"
 	"time"
-
-	logger "github.com/jmuia/tcp-proxy/logging"
 )
 
 type TCPHealthCheck struct {
@@ -16,11 +14,9 @@ func (hc *TCPHealthCheck) Check() error {
 	// TODO: TCP half-open connection.
 	conn, err := net.DialTimeout("tcp", hc.addr, hc.timeout)
 	if err != nil {
-		logger.Infof("%s failed tcp health check", hc.addr)
 		return err
 	}
 	conn.Close()
-	logger.Infof("%s passed tcp health check", hc.addr)
 	return nil
 }
 
